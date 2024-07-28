ae91a466f4f55b0f4304490625d80c8a0d63a1a9
ErikaContreras15 <84852247+ErikaContreras15@users.noreply.github.com>
2024-07-28 18:49:30 -0500

    Add files via upload

---
+++ src/app/componente/actualizar-cliente/actualizar-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './actualizar-cliente.component.html',
+  styleUrl: './actualizar-cliente.component.scss'
+})
+export class ActualizarClienteComponent {
+
+}
---
+++ src/app/componente/lista-prestamo/lista-prestamo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-prestamo',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-prestamo.component.html',
+  styleUrl: './lista-prestamo.component.scss'
+})
+export class ListaPrestamoComponent {
+
+}

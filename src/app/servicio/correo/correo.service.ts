672a416b0bcba6c7448bc72f08043d71fb0a00f3
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-07-28 22:47:06 -0500

    servicio CORREO
    creacion de la clase servicio y conexion con emailJS fallida

--- src/app/servicio/correo/correo.service.ts
+++ src/app/servicio/correo/correo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import * as emailjs from 'emailjs-com';
 @Injectable({
   providedIn: 'root'
 })
 export class CorreoService {
+  constructor() {
+    emailjs.init('kJpPLWuM919m4Nk_K');//API KEYS de mi cuenta de correo de EmailJS
+  }
 
-  constructor() { }
+  sendEmail(toEmail: string, subject: string, message: string): Promise<emailjs.EmailJSResponseStatus> {
+    const templateParams = {
+      to_email: toEmail,
+      subject: subject,
+      message: message
+    };
+
+    return emailjs.send('service_v7f728j', 'template_m0uh7rh', templateParams);
+  }
 }
